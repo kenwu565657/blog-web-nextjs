7f722333976df365e52dc0fdaf63fdedfb239634
kenwu565657 <kenwu565656@icloud.com>
2025-02-25 22:22:00 +0800

    add 3d scene

---
+++ component/3dModelComponent/FarmerSmileScene.tsx
@@ -0,0 +1,48 @@
+import {Canvas, extend, useLoader, useThree} from "@react-three/fiber";
+import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
+import {GLTFLoader} from "three/examples/jsm/loaders/GLTFLoader";
+import {Suspense, useRef} from "react";
+import {Light} from "three";
+import ModelLoadingPlaceHolder from "@/component/3dModelComponent/ModelLoadingPlaceHolder";
+
+extend({ OrbitControls })
+
+function FarmerSmileScene() {
+    const {camera, gl} = useThree();
+    const model = useLoader(GLTFLoader, '/model/farmer_smile.glb');
+    const directionalLight = useRef<Light>(null!);
+
+    return (
+        <group>
+            <scene>
+                <mesh position-y={-2.7} rotation-x={-Math.PI * 0.5} scale={10}>
+                    <planeGeometry/>
+                    <meshStandardMaterial color="greenyellow"/>
+                </mesh>
+                <directionalLight ref={directionalLight} position={[2, 2, 3]} intensity={3}/>
+                <ambientLight intensity={1.5}/>
+                <orbitControls args={[camera, gl.domElement]}/>
+                <primitive object={model.scene}
+                           scale={1.0}
+                           rotation-y={-1.4}
+                           position-x={0}
+                           position-z={0}
+                >
+                </primitive>
+            </scene>
+        </group>
+    )
+}
+
+export default function FarmerSmileSceneCanvas() {
+    return (
+        <Canvas className={"bg-sky-500 h-full"}>
+            <Suspense fallback={<ModelLoadingPlaceHolder scale={3}
+                                                         rotation-x={1}
+                                                         rotation-y={-1}
+            />}>
+                <FarmerSmileScene/>
+            </Suspense>
+        </Canvas>
+    );
+}
--- component/3dModelComponent/FarmingScene.tsx
+++ component/3dModelComponent/FarmingScene.tsx
@@ -2,26 +2,22 @@ import {Suspense, useRef} from "react";
 import {useThree, Canvas, useLoader, useFrame, extend} from "@react-three/fiber";
 import {GLTFLoader} from "three/examples/jsm/loaders/GLTFLoader.js";
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import {Center, Environment, Sky, Text3D, useAnimations, Float} from "@react-three/drei";
-import {useControls} from "leva";
-import {Light, Mesh, CineonToneMapping, ACESFilmicToneMapping, AnimationAction} from "three";
+import {Text3D, useAnimations, Float} from "@react-three/drei";
+import {Light, Mesh, AnimationAction} from "three";
 import ModelLoadingPlaceHolder from "@/component/3dModelComponent/ModelLoadingPlaceHolder";
+import FarmLand from "@/component/3dModelComponent/FarmLand";
 
 extend({ OrbitControls })
 
 function FarmingScene() {
-    const { camera, gl, clock } = useThree();
+    const { camera, gl } = useThree();
     const model = useLoader(GLTFLoader, '/model/farmer_v3.glb');
     const animations = useAnimations(model.animations, model.scene);
     const directionalLight = useRef<Light>(null!);
     const modelRef = useRef<Mesh>(null);
 
-    useFrame((state, delta) => {
-        //const angle = state.clock.elapsedTime;
-        //state.camera.position.x = Math.sin(angle);
-        //state.camera.position.z = Math.cos(angle);
-        //state.camera.lookAt(0, 0, 0);
-
+    useFrame((state) => {
+        state.camera.lookAt(0, 0, 0);
 
         if (modelRef.current) {
             const action: AnimationAction|null = animations.actions[animations.names[1]];
@@ -32,8 +28,8 @@ function FarmingScene() {
     });
 
     return (
-        <group>
             <scene>
+                <FarmLand />
                 <mesh position-z={-5} position-y={4} scale={10}>
                     <planeGeometry/>
                     <meshBasicMaterial color="red"/>
@@ -46,47 +42,38 @@ function FarmingScene() {
                     <planeGeometry/>
                     <meshStandardMaterial color="greenyellow"/>
                 </mesh>
+
                 <directionalLight ref={directionalLight} position={[2, 2, 3]} intensity={3}/>
                 <ambientLight intensity={1.5}/>
                 <orbitControls args={[camera, gl.domElement]}/>
-                <Text3D font="./fonts/helvetiker_regular.typeface.json"
-                        size={ 0.75 }
-                        height={ 0.2 }
-                        position={[-4, 5, 3]}
-                        rotation={[0,Math.PI * 0.2,0]}
-                >
-                    Farm The Best Content
-                    <meshNormalMaterial />
-                </Text3D>
+                <Float>
+                    <Text3D font="./fonts/helvetiker_regular.typeface.json"
+                            size={ 1.0 }
+                            height={ 0.2 }
+                            position={[-5, 5, 3]}
+                            rotation={[0, Math.PI * 0.2, 0]}
+                    >
+                        Farm The Best Content
+                        <meshStandardMaterial color="white"/>
+                    </Text3D>
+                </Float>
+
                 <primitive object={model.scene}
                            ref={modelRef}
-                           scale={0.2}
+                           scale={0.25}
                            rotation-y={1}
                            position-x={-2}
                            position-z={-1}
                 >
                 </primitive>
             </scene>
-        </group>
     )
 }
 
 export default function FarmingSceneCanvas() {
-
-    const {position, sunPosition} = useControls(
-        {
-            position: -2,
-            sunPosition: {value: [0, 0, 0]}
-        },
-    );
-
     return (
-            <Canvas className={"bg-sky-500"}>
-                <Suspense fallback={<ModelLoadingPlaceHolder scale={0.1}
-                                                             rotation-x={0.5}
-                                                             rotation-y={1}
-                                                             position-x={-2}
-                                                             position-y={0.1}/>}>
+            <Canvas className={"bg-sky-500 h-full"} camera={ { fov: 50, near: 1, far: 300, position: [ 0, 2, 9 ] } }>
+                <Suspense fallback={<ModelLoadingPlaceHolder scale={5.0} rotation-x={0.5} rotation-y={0.5}/>}>
                     <FarmingScene/>
                 </Suspense>
             </Canvas>
