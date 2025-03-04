7751e64b5f099bad29d90c7eab19b3eee912439a
kenwu565657 <kenwu565656@icloud.com>
2025-03-04 10:59:40 +0800

    update header style

--- component/Header.tsx
+++ component/Header.tsx
@@ -7,7 +7,7 @@ const Header = () => {
         <div className="relative px-4 py-1 left-0 top:0 bg-white mb-4">
             <div className="items-center justify-between flex flex-col md:flex-row">
                 <Link href="/home">
-                    <CommonImageComponent className="min-w-44 max-w-80" src="/icon/contentFarm_logo_transparent_cutted.png" isLocal={true}></CommonImageComponent>
+                    <CommonImageComponent className="min-w-44 max-w-80" src="/icon/contentFarm_logo_transparent_cutted.png" isLocal={true} width={300}></CommonImageComponent>
                 </Link>
                 <HeaderMenu></HeaderMenu>
             </div>
--- component/HeaderMenu.tsx
+++ component/HeaderMenu.tsx
@@ -2,19 +2,20 @@ import NavigationLink from "@/component/NavigationLink";
 import SearchComponent from "@/component/SearchComponent";
 
 export default function HeaderMenu() {
-    const headerStyle = "flex items-center flex-col md:flex-row md:space-x-8";
+    const headerStyle = "flex items-center flex-col divide-x-1 md:flex-row md:space-x-8 md:divide-y-1";
+    const linkStyle = "hover:font-semibold";
 
     return (
         <div className={headerStyle}>
             <SearchComponent></SearchComponent>
             <NavigationLink href={"/home"}>
-                <span className={headerStyle}>Home Page</span>
+                <span className={linkStyle}>Home Page</span>
             </NavigationLink>
             <NavigationLink href={"/blogpost"}>
-                <span className={headerStyle}>Blog Posts</span>
+                <span className={linkStyle}>Blog Posts</span>
             </NavigationLink>
             <NavigationLink href={"/about"}>
-                <span className={headerStyle}>About Me</span>
+                <span className={linkStyle}>About Me</span>
             </NavigationLink>
         </div>
     );
