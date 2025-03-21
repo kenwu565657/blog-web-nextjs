8e98588d798f15eea211ab320212c57fd17449c5
kenwu565657 <kenwu565656@icloud.com>
2025-03-22 01:58:22 +0800

    update typo

--- component/about/AboutMeDescription.tsx
+++ component/about/AboutMeDescription.tsx
@@ -2,7 +2,7 @@ export function AboutMeDescription() {
     return (
         <div className="p-5">
             <p>Hello, I am Ken, a junior software engineer who are interested in Java and TypeScript full-stack development.</p>
-            <p>Ths website is for me to record some useful knowledge.</p>
+            <p>This website is for me to record some useful knowledge.</p>
         </div>
     )
 }
