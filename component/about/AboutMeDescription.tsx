246ba8ad95ec3dc6c46ca1039cce1017502e97a9
kenwu565657 <kenwu565656@icloud.com>
2025-04-14 23:23:35 +0800

    fix wording

--- component/about/AboutMeDescription.tsx
+++ component/about/AboutMeDescription.tsx
@@ -1,7 +1,7 @@
 export function AboutMeDescription() {
     return (
         <div className="p-5">
-            <p>Hello, I am Ken, a junior software engineer who are interested in Java and TypeScript full-stack development.</p>
+            <p>Hello, I am Ken, a junior software engineer who is interested in Java and TypeScript full-stack development.</p>
             <p>This website is for me to record some useful knowledge.</p>
         </div>
     )
